78b3f6d3ac655d46ba51f5940577471f0d65fbd3
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2009-08-11 03:56:28 +0000

    Changes in attribute API
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@9221 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- ADApp/marCCDSrc/marCCD.cpp
+++ ADApp/marCCDSrc/marCCD.cpp
@@ -252,7 +252,7 @@ void marCCD::getImageData()
     pImage->timeStamp = this->acqStartTime.secPastEpoch + this->acqStartTime.nsec / 1.e9;
 
     /* Get any attributes that have been defined for this driver */        
-    this->getAttributes(pImage);
+    this->getAttributes(pImage->pAttributeList);
 
     /* Call the NDArray callback */
     /* Must release the lock here, or we can get into a deadlock, because we can
