04674fa4ab43b34913b22c3e01fce972d120d3b3
Mark Rivers <rivers@cars.uchicago.edu>
2014-01-27 16:38:25 -0600

    Add requestfile_path for marCCDApp/Db

--- iocs/marCCDIOC/iocBoot/iocMARCCD/st.cmd
+++ iocs/marCCDIOC/iocBoot/iocMARCCD/st.cmd
@@ -33,6 +33,7 @@ dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=I
 
 # Load all other plugins using commonPlugins.cmd
 < $(ADCORE)/iocBoot/commonPlugins.cmd
+set_requestfile_path("$(ADMARCCD)/marCCDApp/Db")
 
 #asynSetTraceMask("$(PORT)",0,3)
 #asynSetTraceIOMask("$(PORT)",0,4)
